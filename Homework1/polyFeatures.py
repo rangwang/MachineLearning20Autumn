034a9bdd366f98116421e142cf4ddf451d656397
rangwang <zhuojunlee724@gmail.com>
2020-10-27 09:00:15 +0800

    删除重复文件

--- Homework1/polyFeatures.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-def polyfeatures(x_data, degree):
-    m = np.size(x_data, 0)
-    x_poly = np.empty([m, degree+1])
-    for i in range(0, m):
-        for j in range(0, degree+1):
-            x_poly[i, j] = np.power(x_data[i, 0], j)
-    return x_poly
